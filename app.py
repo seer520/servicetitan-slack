b976fc5d0e17fe129c0db8bdd24e07fbaeb1a729
seer520 <luis@seerheatingandcooling.com>
2025-04-03 14:39:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,7 @@ def receive_webhook():
     return jsonify({'status': 'Message sent to Slack'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
